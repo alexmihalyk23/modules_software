ab0ff9f4cc5cd4c61483424eb81adaa479006cc1
Alexey Mikhailyuk <35634279+alexmihalyk23@users.noreply.github.com>
2021-11-01 21:29:09 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map1 from './Map1';
+
+function App() {
+  return (
+    <div align="center">
+    {/* <Hosting /> */}
+    <Map1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Map1.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { YMaps, Map, GeolocationControl,FullscreenControl, ZoomControl  } from 'react-yandex-maps'
+
+const Map1 = () => {
+  return (
+    <YMaps>
+  <Map
+    defaultState={{
+      center: [55.018803, 82.933952],
+      zoom: 9,
+      controls: [],
+    }}
+  >
+     <FullscreenControl />
+    //<GeolocationControl options={{ float: 'left' }} />
+    <ZoomControl options={{ float: 'left' }} />
+  </Map>
+</YMaps>
+  );
+};
+
+export default Map1;
