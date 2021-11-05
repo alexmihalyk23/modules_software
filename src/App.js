59a4814b7171c10df4037a48e716338d554e4aff
alexmihalyk <alexmihalyk23@gmail.com>
2021-11-05 13:27:04 +0700

    add features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Map1 from './Map1';
 import DownloadApp from './DownloadApp';
+// import GISmap from "./GISmap";
 
 function App() {
   return (
     <div align="center">
     {/* <Hosting /> */}
-    <Map1 />
+      <Map1 />
       <DownloadApp/>
+        {/*<GISmap></GISmap>*/}
     </div>
   );
 }
--- src/Map1.js
+++ src/Map1.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { YMaps, Map, GeolocationControl,FullscreenControl, ZoomControl  } from 'react-yandex-maps'
+import { YMaps, Map,FullscreenControl, ZoomControl  } from 'react-yandex-maps'
 
 const Map1 = () => {
-  return (
-    <YMaps>
-  <Map
-    defaultState={{
-      center: [55.018803, 82.933952],
-      zoom: 9,
-      controls: [],
-    }}
-  >
-     <FullscreenControl />
-    {/*<GeolocationControl options={{ float: 'left' }} />*/}
-    <ZoomControl options={{ float: 'left' }} />
-  </Map>
-</YMaps>
-  );
+    return (
+        <YMaps>
+            <Map
+                defaultState={{
+                    center: [55.018803, 82.933952],
+                    zoom: 9,
+                    controls: [],
+                }}
+            >
+                <FullscreenControl />
+                <ZoomControl options={{ float: 'left' }} />
+            </Map>
+        </YMaps>
+    );
 };
 
 export default Map1;
